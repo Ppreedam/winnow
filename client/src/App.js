cd925ebb599a6b16d75022691552788182b8bcfc
ppreedam <sk6201184579@gmail.com>
2023-06-28 08:56:25 +0530

    Add changes

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,8 @@ function App() {
     <>
     <div>
       {/* <CollapsibleExample /> */}
-      {/* <AllRoutes/> */}
-      <ProductDescription/>
+      <AllRoutes/>
+      {/* <ProductDescription/> */}
       {/* <Account/> */}
       {/* <Portfolio/> */}
       {/* <Wallet/> */}
