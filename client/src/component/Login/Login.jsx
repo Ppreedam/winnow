efed6b9918be50859931b759d3b74321ebfb4040
Rabi3421 <rabinarayan3421@gmail.com>
2023-07-02 08:01:45 +0530

    updated

--- client/src/component/Login/Login.jsx
+++ client/src/component/Login/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     setLoading(true)
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:8000/api/v1/auth/login", {
+      const res = await axios.post("https://winnow-backend-api.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
