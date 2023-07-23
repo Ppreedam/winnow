04a5515009a52d5ac74a53b1cf4dd39946c18a6d
Rabi3421 <rabinarayan3421@gmail.com>
2023-07-23 11:06:43 +0530

    changes done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,10 @@
 import AllRoutes from "./component/AllRoutes/AllRoutes";
-
-import Winow from "./component/HomemainPage/Winow";
-import Wallet from "./component/MyWallet/Wallet";
-import CollapsibleExample from "./component/Navbar/Navbar";
-// import { PieChart } from "./component/Portfoio/Piacharts";
-import Portfolio from "./component/Portfoio/Portfolio";
-import ProductDescription from "./component/ProductDescriptionPage/ProductDescription";
-// import ProgressBar  from "./component/ProgressBar/ProgressBar";
-
-
-
-
-  
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
     <div className="mainRoute">
       <AllRoutes />
-
     </div>
   );
 }
--- client/src/component/AllRoutes/AllRoutes.jsx
+++ client/src/component/AllRoutes/AllRoutes.jsx
@@ -1,78 +1,49 @@
 import React from "react";
-// import style from "./AllRoutes.module.css";
 import { Route, Routes } from "react-router-dom";
-// import { Route, Routes } from "react-router-dom";
-
 import Winow from "../HomemainPage/Winow";
-import Register from "../Register/Register";
-import Login from "../Login/Login";
-
-// import Dashboard from "../../pages/Admin/Dashboard/Dashboard"
-// import Users from "../../pages/Admin/Users/Users"
-// import Category from "../../pages/Admin/Category/Category"
-// import CreateProducts from "../../pages/Admin/CreateProducts/CreateProducts"
-// import AllProducts from "../../pages/Admin/AllProducts/AllProducts"
-
-// import PrivateRoute from "../Routes/Private";
-
-
-import AdminRoute from "../Routes/AdminRoute";
-
-
 import Dashboard from "../../pages/Admin/Dashboard/Dashboard";
 import Users from "../../pages/Admin/Users/Users";
 import Category from "../../pages/Admin/Category/Category";
 import CreateProducts from "../../pages/Admin/CreateProducts/CreateProducts";
 import AllProducts from "../../pages/Admin/AllProducts/AllProducts";
-
-// import PrivateRoute from "../Routes/Private";
-
-// import AdminRoute from "../Routes/AdminRoute";
 import Comingsoon from "../ComingSoon/Comingsoon";
 import EditProfile from "../EditProfile/EditProfile";
 import Portfolio from "../Portfoio/Portfolio";
 import Wallet from "../MyWallet/Wallet";
 import Account from "../Account/Account";
-
 import InvestNow from "../HomemainPage/InvestNow";
 import PrivateRoute from "../Higherorderfunction/PrivateRoute";
-
 import ForgotPasssword from "../Login/ForgetPassword";
 import CreateProducts2 from "../../pages/Admin/CreateProducts/CreateProducts2";
 import ProductDescription from "../ProductDescriptionPage/ProductDescription";
 import Demoligin from "../Login/Demoligin";
 import Demosigup from "../Register/Demosigup";
 
-// import AdminRoutes from "../../pages/Admin/AdminRoutes/AdminRoutes";
-
 const AllRoutes = () => {
   return (
     <div>
-      {/* <Sidebar />
-      <Routes >
-        <Route exact path="/" element={<Dashboard/>}/>
-        <Route exact path="/user" element={<Users/>} />
-        <Route exact path = "/category" element={<Category/>} />
-        <Route exact path = "/createproduct" element={<CreateProducts/>} />
-        <Route exact path = "/allproducts" element={<AllProducts/>} />
-      </Routes> */}
       <Routes>
         <Route exact path="/" element={<Winow />} />
         <Route exact path="/user/login" element={<Demoligin />} />
         <Route exact path="/user/register" element={<Demosigup />} />
-
-        <Route exact path="/user/gorget-password" element={<ForgotPasssword />} />
+        <Route
+          exact
+          path="/user/gorget-password"
+          element={<ForgotPasssword />}
+        />
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/comingsoon" element={<Comingsoon />} />
-
         <Route exact path="/comingsoon" element={<Comingsoon />} />
         <Route exact path="/editprofile" element={<EditProfile />} />
         <Route exact path="/portfolio" element={<Portfolio />} />
         <Route exact path="/mywallet" element={<Wallet />} />
         <Route exact path="/account" element={<Account />} />
-        <Route exact path="/formproduct" element={<CreateProducts/>} />
-        <Route exact path="/productDesc/:slug" element={<ProductDescription/>}/>
-        {/* <Route exact path="/admin" element={<AdminRoutes />} /> */}
+        <Route exact path="/formproduct" element={<CreateProducts />} />
+        <Route
+          exact
+          path="/productDesc/:slug"
+          element={<ProductDescription />}
+        />
         <Route
           exact
           path="/user/gorget-password"
@@ -132,11 +103,9 @@ const AllRoutes = () => {
             </PrivateRoute>
           }
         />
-
       </Routes>
     </div>
   );
 };
-// className={style.AppGlass}
 
 export default AllRoutes;
