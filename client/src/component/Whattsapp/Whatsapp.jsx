5d26c2576d5b4640abae95f6c6df67665d0d9793
Rabi3421 <rabinarayan3421@gmail.com>
2023-07-26 23:39:55 +0530

    final submission

--- client/src/component/Whattsapp/Whatsapp.jsx
+++ client/src/component/Whattsapp/Whatsapp.jsx
@@ -5,7 +5,7 @@ import { FaWhatsappSquare } from "react-icons/fa";
 const Whatsapp = () => {
   return (
     <div className={style.container}>
-        <a href='https://wa.me/916201184579' target='_blank'>
+        <a href='https://wa.me/917003424150' target='_blank'>
             <FaWhatsappSquare className={style.whatsapp_icon}/>
         </a>
     </div>
